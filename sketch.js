105f786814d04dac8f110bdac9572dfb4f91b96d
chepecarlos <chepecarlos@alswblog.org>
2019-10-02 19:31:19 -0600

    Codigo complejo agrado mqtt y p5

--- sketch.js
+++ sketch.js
@@ -1,12 +1,73 @@
+let BotonActivar;
+let BotonApagar;
+let EstadoFondo = false;
+
+let BrokerMQTT = 'broker.shiftr.io';
+let PuertoMQTT = 80;
+let ClienteIDMQTT = "Pagina-" + Math.floor(Math.random() * 1000);
+let UsuarioMQTT = "polloALSW";
+let ContrasenaMQTT = "PolloSecreto";
+
+client = new Paho.MQTT.Client(BrokerMQTT, PuertoMQTT, ClienteIDMQTT);
+
+client.onConnectionLost = MQTTPerder;
+client.onMessageArrived = MQTTMensaje;
+
+client.connect({
+  onSuccess: CuandoConectadoMQTT,
+  userName: UsuarioMQTT,
+  password: ContrasenaMQTT
+});
+
+function MQTTPerder(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("MQTT Perdio coneccion Error:" + responseObject.errorMessage);
+  }
+}
+
+function MQTTMensaje(message) {
+  console.log("Mensaje recibido:" + message.payloadString);
+  let Mensaje = message.payloadString;
+  if (Mensaje == '1') {
+    EstadoFondo = true;
+    console.log("Encendiendo Fondo")
+  } else {
+    EstadoFondo = false;
+  }
+}
+
+function CuandoConectadoMQTT() {
+  console.log("MQTT Conectado");
+  client.subscribe("/ALSW/Boton");
+}
+
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(200, 200);
+  createP();
+  BotonActivar = createButton("Activar Led");
+  BotonApagar = createButton("Apagar Led");
+  BotonActivar.mousePressed(ActivarLed);
+  BotonApagar.mousePressed(ApagarLed);
+}
+
+function ApagarLed() {
+  console.log("Apagnado Led");
+  message = new Paho.MQTT.Message("0");
+  message.destinationName = "/ALSW/Led";
+  client.send(message);
+}
+
+function ActivarLed() {
+  console.log("Encender Led");
+  message = new Paho.MQTT.Message("1");
+  message.destinationName = "/ALSW/Led";
+  client.send(message);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
+  if (EstadoFondo) {
+    background(0);
   } else {
-    fill(255);
+    background(255);
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
